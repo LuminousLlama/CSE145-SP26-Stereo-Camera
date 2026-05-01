1a38a556dd6ff3eb34f91e47c4e5db2582fb9b8a
Sumukh Murthy <68134973+Augustus31@users.noreply.github.com>
2026-05-01 04:54:37 -0700

    unit tests & optimization for 60 fps

--- workspace_test/src/pointcloud_pub/pointcloud_pub/pointcloud_pub.py
+++ workspace_test/src/pointcloud_pub/pointcloud_pub/pointcloud_pub.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import time
+
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import PointCloud2, PointField
--- workspace_test/src/pointcloud_pub/pointcloud_pub/stereo.py
+++ workspace_test/src/pointcloud_pub/pointcloud_pub/stereo.py
@@ -4,6 +4,7 @@ import numpy as np
 import cv2
 import unittest
 import random
+#from matplotlib import pyplot as plt
 
 # import os
 # print(os.getcwd())
